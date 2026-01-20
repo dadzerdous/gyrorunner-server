e1e21d4c66d3836a4b308ed345490aa4d060c7ed
dadzerdous <dadzaverous@gmail.com>
2026-01-19 22:19:33 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,38 @@
+import { WebSocketServer } from "ws";
+
+const PORT = process.env.PORT || 3000;
+const wss = new WebSocketServer({ port: PORT });
+
+const players = {};
+
+wss.on("connection", (ws) => {
+  const id = Math.random().toString(36).slice(2);
+
+  players[id] = { x: 0, y: 0, hp: 10 };
+
+  ws.on("message", (data) => {
+    const msg = JSON.parse(data);
+
+    if (msg.type === "move") {
+      players[id].x += msg.x;
+      players[id].y += msg.y;
+    }
+  });
+
+  ws.on("close", () => {
+    delete players[id];
+  });
+});
+
+setInterval(() => {
+  const snapshot = JSON.stringify({
+    type: "state",
+    players
+  });
+
+  wss.clients.forEach(c => {
+    if (c.readyState === 1) c.send(snapshot);
+  });
+}, 50);
+
+console.log("GyroRunner server running on port", PORT);
